f3cc8a3d20b27bcd7e04fd485a4ddf7c33bb0ec1
Pavel Itchin <itchin.pavel@yandex.ru>
2021-08-09 22:05:42 +0300

    Update movie-card.js

--- src/view/movie-card.js
+++ src/view/movie-card.js
@@ -1,4 +1,4 @@
-import { createElement } from '../utils/utils';
+import AbstractView from './abstract';
 
 const createMovieCardTemplate = (movie) => (
   `<article class="film-card">
@@ -20,25 +20,13 @@ const createMovieCardTemplate = (movie) => (
   </article>`
 );
 
-export default class MovieCard {
+export default class MovieCard extends AbstractView {
   constructor(movie) {
+    super();
     this._movie = movie;
-    this._element = null;
   }
 
   getTemplate() {
     return createMovieCardTemplate(this._movie);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
