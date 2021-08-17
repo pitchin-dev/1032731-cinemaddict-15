ee608730d4cb59b2baaddc273fc60ef5ce6613a7
Pavel Itchin <itchin.pavel@yandex.ru>
2021-08-17 21:45:54 +0300

    Update movie-list.js

--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -17,15 +17,19 @@ export default class MovieList {
     this._mainContainer = mainContainer;
     this._movies = movies.slice();
     this._moviesByDefault = movies.slice();
-    this._moviesMostCommented = this._movies.sort((a, b) => b.comments.length - a.comments.length).slice(0, 2);
-    this._moviesTopRated = this._movies.sort((a, b) => b.rating - a.rating).slice(0, 2);
+    this._moviesMostCommented = this._movies.slice().sort((a, b) => b.comments.length - a.comments.length).slice(0, 2);
+    this._moviesTopRated = this._movies.slice().sort((a, b) => b.rating - a.rating).slice(0, 2);
     this._buttons = buttons;
 
     this._filmsCounter = FILM_BLOCK_SIZE;
-    this._movieListPresenter = new Map();
+    this._movieListPresenterMain = new Map();
+    this._movieListPresenterRated = new Map();
+    this._movieListPresenterCommented = new Map();
 
     this._showMoreMovies = this._showMoreMovies.bind(this);
-    this._handleMovieChange = this._handleMovieChange.bind(this);
+    this._handleMainMovieChange = this._handleMainMovieChange.bind(this);
+    this._handleRatedMovieChange = this._handleRatedMovieChange.bind(this);
+    this._handleCommentedMovieChange = this._handleCommentedMovieChange.bind(this);
   }
 
   init() {
@@ -35,10 +39,22 @@ export default class MovieList {
     this._showMoreMovies();
   }
 
-  _handleMovieChange(updatedMovie) {
+  _handleMainMovieChange(updatedMovie) {
     this._movies = updateItem(this._movies, updatedMovie);
     this._sourcedBoardTasks = updateItem(this._moviesByDefault, updatedMovie);
-    this._movieListPresenter.get(updatedMovie.id).init(updatedMovie);
+    this._movieListPresenterMain.get(updatedMovie.id).init(updatedMovie);
+  }
+
+  _handleRatedMovieChange(updatedMovie) {
+    this._movies = updateItem(this._movies, updatedMovie);
+    this._sourcedBoardTasks = updateItem(this._moviesByDefault, updatedMovie);
+    this._movieListPresenterRated.get(updatedMovie.id).init(updatedMovie);
+  }
+
+  _handleCommentedMovieChange(updatedMovie) {
+    this._movies = updateItem(this._movies, updatedMovie);
+    this._sourcedBoardTasks = updateItem(this._moviesByDefault, updatedMovie);
+    this._movieListPresenterCommented.get(updatedMovie.id).init(updatedMovie);
   }
 
   _renderMainElements() {
@@ -74,16 +90,28 @@ export default class MovieList {
     }
   }
 
-  _renderMovieCard(container, movie) {
-    const card = new MovieCard(container, this._handleMovieChange);
-    this._movieListPresenter.set(movie.id, card);
+  _renderMovieCardMain(container, movie) {
+    const card = new MovieCard(container, this._handleMainMovieChange);
+    this._movieListPresenterMain.set(movie.id, card);
+    card.init(movie);
+  }
+
+  _renderMovieCardRated(container, movie) {
+    const card = new MovieCard(container, this._handleRatedMovieChange);
+    this._movieListPresenterRated.set(movie.id, card);
+    card.init(movie);
+  }
+
+  _renderMovieCardCommented(container, movie) {
+    const card = new MovieCard(container, this._handleCommentedMovieChange);
+    this._movieListPresenterCommented.set(movie.id, card);
     card.init(movie);
   }
 
   _renderMovieCards() {
-    this._movies.slice(0, FILM_BLOCK_SIZE).forEach((movie) => this._renderMovieCard(this._movieListBlockMainContainer, movie));
-    this._moviesTopRated.forEach((movie) => this._renderMovieCard(this._movieListBlockTopRatedContainer, movie));
-    this._moviesMostCommented.forEach((movie) => this._renderMovieCard(this._movieListBlockMostCommentedContainer, movie));
+    this._movies.slice(0, FILM_BLOCK_SIZE).forEach((movie) => this._renderMovieCardMain(this._movieListBlockMainContainer, movie));
+    this._moviesTopRated.forEach((movie) => this._renderMovieCardRated(this._movieListBlockTopRatedContainer, movie));
+    this._moviesMostCommented.forEach((movie) => this._renderMovieCardCommented(this._movieListBlockMostCommentedContainer, movie));
   }
 
   _showMoreMovies() {
