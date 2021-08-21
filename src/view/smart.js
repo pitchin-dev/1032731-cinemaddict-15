b3618470952665c7a53a6a9e0d610f9a0b26219f
Pavel Itchin <itchin.pavel@yandex.ru>
2021-08-21 20:34:14 +0300

    Update smart.js

--- src/view/smart.js
+++ src/view/smart.js
@@ -1,22 +1,40 @@
-import { replaceComponent } from '../utils/render';
 import AbstractView from './abstract';
 
 export default class SmartView extends AbstractView {
   constructor () {
     super();
+    this._data = {};
   }
 
   restoreHandlers() {
     throw new Error('Cannot initialize this method by the abstract class: restoreHandlers');
   }
 
-  _updateElement() {
+  updateElement() {
     const prevElement = this.getElement();
     const parent = prevElement.parentElement;
     this.removeElement();
 
     const newElement = this.getElement();
-    parent.replaceComponent(newElement, prevElement);
+    parent.replaceChild(newElement, prevElement);
+
+    this.restoreHandlers();
+  }
+
+  updateData(update, noRerender) {
+    if (!update) {
+      return;
+    }
+
+    this._data = Object.assign(
+      {},
+      this._data,
+      update,
+    );
+
+    if (noRerender) {
+      return;
+    }
 
     this.restoreHandlers();
   }
