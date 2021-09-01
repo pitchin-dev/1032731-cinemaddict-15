75dc490ffbd9d65213a02205214e0a7d4e8ef18f
Pavel Itchin <itchin.pavel@yandex.ru>
2021-09-01 11:58:28 +0300

    Update movie-popup.js

--- src/view/movie-popup.js
+++ src/view/movie-popup.js
@@ -237,7 +237,7 @@ export default class MoviePopupView extends SmartView {
       ...movie,
       addedComment: null,
       addedEmoji: null,
-      hasComments: movie.comments.length > 0
+      hasComments: movie.comments.length > 0,
     };
   }
 
