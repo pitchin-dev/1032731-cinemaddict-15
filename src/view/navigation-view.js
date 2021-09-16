553651f1c7c1653550d2045a94c748a2e7d3df00
Pavel Itchin <itchin.pavel@yandex.ru>
2021-09-17 01:09:36 +0300

    Update navigation-view.js

--- src/view/navigation-view.js
+++ src/view/navigation-view.js
@@ -8,7 +8,38 @@ const createNavigationTemplate = (isTarget) => (
 );
 
 export default class NavigationView extends AbstractView {
+  constructor() {
+    super();
+
+    this._isTarget = false;
+    this._navigationClickHandler = this._navigationClickHandler.bind(this);
+  }
+
   getTemplate() {
-    return createNavigationTemplate();
+    return createNavigationTemplate(this._isTarget);
+  }
+
+  setNavigationClickHandler(callback) {
+    this._callback.navigationClick = callback;
+    this.getElement().addEventListener('click', this._navigationClickHandler);
+  }
+
+  _navigationClickHandler(evt) {
+    if (evt.target.tagName !== 'A') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._isTarget = evt.target.matches('.main-navigation__additional');
+    this._toggleActiveClass(this._isTarget);
+    this._callback.navigationClick(this._isTarget);
+  }
+
+  _toggleActiveClass(isTarget) {
+    const statsLinkElement = this.getElement().querySelector('.main-navigation__additional');
+
+    isTarget
+      ? statsLinkElement.classList.add('main-navigation__additional--active')
+      : statsLinkElement.classList.remove('main-navigation__additional--active');
   }
 }
