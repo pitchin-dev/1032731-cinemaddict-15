b2bf82e2a4beee69a69b8fd5a74c4fbe897b8a60
Pavel Itchin <itchin.pavel@yandex.ru>
2021-09-17 11:48:28 +0300

    Create movies-container-view.js

---
+++ src/view/movies-container-view.js
@@ -0,0 +1,23 @@
+import AbstractView from './abstract-view';
+import {MAIN_TITLE} from '../const';
+
+const createMoviesTemplate = (moviesCount, title) => (
+  `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title ${(moviesCount) ? 'visually-hidden' : ''}">${(moviesCount) ? MAIN_TITLE : `${title}`}</h2>
+      ${(moviesCount) ? '<div class="films-list__container"></div>' : ''}
+    </section>
+  </section>`
+);
+
+export default class FilmsContainerView extends AbstractView {
+  constructor(moviesCount, title) {
+    super();
+    this._moviesCount = moviesCount;
+    this._title = title;
+  }
+
+  getTemplate() {
+    return createMoviesTemplate(this._moviesCount, this._title);
+  }
+}
